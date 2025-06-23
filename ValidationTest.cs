54dcd2ecc6acc825d8f83c067fbe8d639c7b5495
KasiaOT <167691567+katarzynaOT@users.noreply.github.com>
2025-06-24 01:57:37 +0200

    testowanie pdf

---
+++ ValidationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkshopManager.Models;
+using WorkshopManager.Models;
+
+namespace WorkshopManagerTests
+{
+    public class ValidationTest
+    {
+        [Fact]
+        public void ServiceOrder_Price_Should_Be_Positive()
+        {
+            var order = new ServiceOrder { Price = -100 };
+
+            bool isValid = order.Price >= 0;
+
+            Assert.False(isValid);
+        }
+
+    }
+}
